23b4735f38da281886e2d83a2e55b6904e10d846
Nathan Jensby <njensby@gmail.com>
2016-10-27 10:58:51 -0500

    commented out cors configuration in lists route

--- routes/lists.js
+++ routes/lists.js
@@ -5,19 +5,19 @@ var List = require("../models/list");
 var foods = require('./foods');
 
 // Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
+// app.use(function (req, res, next) {
+//     // Website you wish to allow to connect
+//     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 router.use('/', function (req, res, next) {
   req.body = _.pick(req.body, ['listName', 'foods', 'listOwner', 'sharedOwners'])
